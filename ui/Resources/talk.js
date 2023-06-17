5d5a77fb4d37b04a6d4275c50d687f4a3ff72471
mr1Michael <130656746+mr1Michael@users.noreply.github.com>
2023-06-17 15:57:21 +0200

    final debugging.

--- ui/Resources/talk.js
+++ ui/Resources/talk.js
@@ -2,7 +2,7 @@ const express = require('express');
 const app = express();
 const http = require('http');
 const server = http.createServer(app);
-const { Server } = require("socket.io");
+const {Server} = require("socket.io");
 const kws = require('./KeyWords.js');
 const fs = require("fs");
 let timeoutId;
@@ -10,12 +10,12 @@ let first_go = true;
 const path = require('path')
 
 
-const io = new Server(server, {
-    cors: { //this stays because It is useful
-        origin: "http://localhost:3000",
-        methods: ["GET", "POST"]
-    }
-});
+const io = new Server(server)//,{
+//     cors: { //this stays because It is useful
+//         origin: "http://localhost:3000",
+//         methods: ["GET", "POST"]
+//     }
+// });
 //change the socket port here for front end
 server.listen(5001, () => {
     console.log('listening on *:5001');
@@ -23,39 +23,43 @@ server.listen(5001, () => {
 
 app.use(express.static(path.join(__dirname, 'build')));
 app.get('/', (req, res) => {
-    res.sendFile(path.join(__dirname,  'build','index.html'));
+    res.sendFile(path.join(__dirname, 'build', 'index.html'));
 });
 
 io.on('connect', (socket) => {
     console.log('socket connected');
-    setTimeout(function() {
-        socket.emit('serverMessage','My name is AGIM, I\'m here to help you learn how to play chess');
+    setTimeout(function () {
+        socket.emit('serverMessage', 'My name is AGIM, I\'m here to help you learn how to play chess');
     }, 500);
-    setTimeout(function() {
-        socket.emit('serverMessage','I can also record your games (not againt me), just ask me to ' +
-        '"start recording" and "stop recording" to stop and "review game" to see your moves');
+    setTimeout(function () {
+        socket.emit('serverMessage', 'I can also record your games (not againt me), just ask me to ' +
+            '"start recording" and "stop recording" to stop and "review game" to see your moves');
     }, 500);
-    setTimeout(function() {
-        socket.emit('serverMessage','would you like me to start with the basics?');
+    setTimeout(function () {
+        socket.emit('serverMessage', 'would you like me to start with the basics?');
     }, 500);
 
     socket.on('clientMessage', (data) => {
         console.log('received from client: ' + data);
-        if (first_go){
-            setTimeout(function() {
-                kws.converstaion_handler(data).then((result)=>{
+        if (!first_go) {
+            setTimeout(function () {
+                kws.converstaion_handler(data).then((result) => {
                     socket.emit('serverMessage', result);
                 })
             }, 500);
             first_go = false;
+        }else {
+            kws.converstaion_handler(data).then((result) => {
+                socket.emit('serverMessage', result);
+            });
         }
-        kws.converstaion_handler(data).then((result)=>{
-            socket.emit('serverMessage', result);
-        })
+
     });
     socket.on('disconnect', (socket) => {
-        console.log('socket disconnected');})
+        console.log('socket disconnected');
+    })
 });
 io.on('disconnect', (socket) => {
-    console.log('socket disconnected');})
+    console.log('socket disconnected');
+})
 
