ca7f0719c950403d6610efd5de777fa64dc223c2
mr1Michael <130656746+mr1Michael@users.noreply.github.com>
2023-06-21 14:08:53 +0200

    touch ups

--- ui/Resources/KeyWords.js
+++ ui/Resources/KeyWords.js
@@ -14,7 +14,7 @@ let board_record = [];
 const grammar = ["can", "is", "does", "will", "should", "could", "would", "how", "why", "when", "what", "which", "will",
     "mean"]
 const instruction = ["record", "game", "review", "opening", "recording", "start", "old", "stop", "end", "begin",
-    "agim", "hi", "hello", "ola", "hie", "bonjour"," greetings", "hallo"]
+    "agim", "hi", "hello", "ola", "hie", "bonjour", " greetings", "hallo"]
 const piece = ["king", "queen", "bishop", "knight", "castle", "pawn"];
 const piece_operator = ["move", "capture", "captured", "many", "start", "position", "place", "placed", "worth",
     "value", "direction", "put", "special", "points"];
@@ -24,24 +24,24 @@ let bot_raise = false;
 let recording = false
 let last_question = [];
 
-async function conversation_handler(sentence=""){
-    let words = sentence.toLowerCase().replace(/[!@#$%^&*()+=<>?:"{},./;]/g,"").split(" ");
+async function conversation_handler(sentence = "") {
+    let words = sentence.toLowerCase().replace(/[!@#$%^&*()+=<>?:"{},./;]/g, "").split(" ");
     let kw = find_keywords(words);
     // Bot questions and human answers and bot questions.---------------------------------
     let bot = bot_response(words);
-    if (!(bot === "")){
+    if (!(bot === "")) {
         return bot;
     }
     // Bot questions finished -------------------------------------------------------------
     // Human questions
-    if (recording && bot_is_move(words)){
+    if (recording && bot_is_move(words)) {
         return "move recorded"
     }//Check if it is a move and the move is recorded:------------------
-    if (kw["size"] < 1){
+    if (kw["size"] < 1) {
         return questions["not_understanding"][any_element(questions["not_understanding"].length)];
     }// If keywords too few, ask repeat
     bot = bot_instruction(kw["instruction"]);
-    if (!(bot === "")){
+    if (!(bot === "")) {
         return bot;
     }// Check if it is an instruction and act accordingly
     let find = response.answer(kw)
@@ -49,103 +49,110 @@ async function conversation_handler(sentence=""){
     return find
 } //handles all that chats and assigns them accordingly
 
-function find_keywords(words){
+function find_keywords(words) {
     let keywords = {"piece": [], "piece operator": [], "rule_book": [], "instruction": [], "size": 0}
     let t = [];
-    for (let word of words){
+    for (let word of words) {
         if (piece.includes(word)) {
             keywords["piece"].push(word);
             keywords["size"]++;
-        }if(piece_operator.includes(word)){
+        }
+        if (piece_operator.includes(word)) {
             keywords["piece operator"].push(word);
             keywords["size"]++;
-        }if(rule_book.look.includes(word)) {
+        }
+        if (rule_book.look.includes(word)) {
             keywords["rule_book"].push(word);
             keywords["size"]++;
-        }if(instruction.includes(word)){
+        }
+        if (instruction.includes(word)) {
             keywords["instruction"].push(word);
             keywords["size"]++;
-        // }if ((/[a-h]/i.test(word[0]) )&& (/[1-8]/i.test(word[1]))) {
-        //     // need to decide!!
+            // }if ((/[a-h]/i.test(word[0]) )&& (/[1-8]/i.test(word[1]))) {
+            //     // need to decide!!
         }
     }
     return keywords
 }//splits out keywords according to function
 
 
-function bot_is_move(words){
-    let step = ["","","",""];
+function bot_is_move(words) {
+    let step = ["", "", "", ""];
     let j = 0;
-    for (let word of words){
-        if ((/[a-h]/i.test(word[0])) && (/[1-8]/i.test(word[1])) && (j < 2)){
+    for (let word of words) {
+        if ((/[a-h]/i.test(word[0])) && (/[1-8]/i.test(word[1])) && (j < 2)) {
             step[j] = word;
             j++;
-        } else if ((word === "white") || (word==="Black")){
+        } else if ((word === "white") || (word === "Black")) {
             step[2] = word.charAt(0).toUpperCase() + word.slice(1);
-        } else if (piece.includes(word) ){
+        } else if (piece.includes(word)) {
             step[3] = word.charAt(0).toUpperCase() + word.slice(1);
         }
     }
-    if (["castle", "castling"].some(val => words.includes(val))&& j > 0) {
+    if (["castle", "castling"].some(val => words.includes(val)) && j > 0) {
         return board_record = (board.make_move(["castling", step]));
-    } else if (step[0] !== "" && step[1] !== "" && step[2] !== "" && step[3] !== ""){
+    } else if (step[0] !== "" && step[1] !== "" && step[2] !== "" && step[3] !== "") {
         return board_record = (board.make_move(["", step]));
     } else return null;
 }//Checks if it is a move
-function bot_instruction(instr){
+function bot_instruction(instr) {
     //const instruction = ["record", "game", "review", "opening", "recording", "start", "old", "stop", "end", "begin"]
-    if (instr.length >= 2){
-        if(["start", "begin"].some(val => instr.includes(val)) &&
-            ["recording", "record", "game" ].some(val => instr.includes(val))){
+    if (instr.length >= 2) {
+        if (["start", "begin"].some(val => instr.includes(val)) &&
+            ["recording", "record", "game"].some(val => instr.includes(val))) {
             recording = true;
             return "I am now recording the moves you pass me. Remember I need this format: 'piece name' 'origin sqaure'" +
                 " 'destination square'. \n In the case of castling, just say 'castle', then 'castle square'";
-        }if(["stop", "end"].some(val => instr.includes(val)) &&
-            ["recording", "record", "game" ].some(val => instr.includes(val))){
+        }
+        if (["stop", "end"].some(val => instr.includes(val)) &&
+            ["recording", "record", "game"].some(val => instr.includes(val))) {
             recording = false;
             return "I am no-longer recording. to start again, you just need to ask 'start recording'";
-        }if(["opening"].some(val => instr.includes(val))) {
+        }
+        if (["opening"].some(val => instr.includes(val))) {
             return questions["positive_answer"][any_element(questions["positive_answer"].length)] +
                 response.bot_answer("A5");
-        }if (["review", "game"].every(val => instr.includes(val))) {
-            return board_record[1];
-        }if (["agim", "hi", "hello", "ola", "hie", "bonjour"," greetings", "hallo"].every(val => instr.includes(val))) {
-            return response.bot_answer("A99");
         }
+        if (["review", "game"].every(val => instr.includes(val))) {
+            return board_record[1];
+        }
+    }
+    if (["agim", "hi", "hello", "ola", "hie", "bonjour", " greetings", "hallo"].some(val => instr.includes(val))) {
+        return response.bot_answer("A99");
     }
     return "";
 }//instructions, mainly for openings and or recording
 
-function bot_response(words){
-    if (words[0] === ""){
+function bot_response(words) {
+    if (words[0] === "") {
         return questions["probe"][any_element(questions["probe"].length)]
-    }
-    else if (words[0] === "_"){
+    } else if (words[0] === "_") {
         bot_question = (bot_question + 1) % questions["yes_or_no"].length;
         bot_raise = true;
         return questions["yes_or_no"][bot_question - 1]; //see what happens when this rolls over at length and this value becomes -1
-    } if (bot_raise){
-        if (["yes", "ja", "yup", "da", "yebo"].some(val => words.includes(val))){
+    }
+    if (bot_raise) {
+        if (["yes", "ja", "yup", "da", "yebo"].some(val => words.includes(val))) {
             bot_raise = false;
-            return response.bot_answer("A"+bot_question)
-        } else if (["no", "nope", "nein", "nada", "na", "ayewa"].some(val => words.includes(val))){
+            return response.bot_answer("A" + bot_question)
+        } else if (["no", "nope", "nein", "nada", "na", "ayewa"].some(val => words.includes(val))) {
             bot_raise = false;
             return questions["enquire"][any_element(questions["enquire"].length)]
         }
-        } else {
-            if (["yes", "ja", "yup", "da", "yebo", "ok", "okay", "sure", "cool", "so"].some(val => words.includes(val))) {
-                if (!first_go) {
-                    return response.next_ans();
-                } else {
-                    first_go = false;
-                    let t = 'Chess is a strategy board game for two players, called White and Black,' +
-                            ' each controlling an army of chess pieces (16) in their color, with the objective to checkmate the' +
-                            ' opponent\'s king. '
-                    return t + bot_response("_");
-                }
+    } else {
+        if (["yes", "ja", "yup", "da", "yebo", "ok", "okay", "sure", "cool", "so"].some(val => words.includes(val))) {
+            if (!first_go) {
+                return response.next_ans();
             } else {
-                if(["no", "nope", "nein", "nada", "na", "ayewa"].some(val => words.includes(val))){
-                    return bot_response("_");
+                first_go = false;
+                let t = 'Chess is a strategy board game for two players, called White and Black,' +
+                    ' each controlling an army of chess pieces (16) in their color, with the objective to checkmate the' +
+                    ' opponent\'s king. '
+                return t + bot_response("_");
+            }
+        } else {
+            if (["no", "nope", "nein", "nada", "na", "ayewa"].some(val => words.includes(val))) {
+                return bot_response("_");
             }
         }
     }
@@ -153,7 +160,7 @@ function bot_response(words){
 } //Automated responses to automated predefined questions
 
 
-function any_element(arr_len){
+function any_element(arr_len) {
     return Math.floor(Math.random() * arr_len);
 } //Chooses random element in anny array
 
